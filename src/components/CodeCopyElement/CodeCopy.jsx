2377979fdd88fec27521b06682fc8ab31d190e9c
pedrosavio <pedro_savio777@hotmail.com>
2022-11-17 10:50:40 -0300

    Base code

--- src/components/CodeCopyElement/CodeCopy.jsx
+++ src/components/CodeCopyElement/CodeCopy.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-
 export const CodeCopyDiv = styled.div`
   .left {
     margin-left: 10rem;
@@ -9,15 +8,69 @@ export const CodeCopyDiv = styled.div`
   h1 {
     text-align: center;
   }
-  justify-content: center;
-  align-items: center;
   color: white;
-  height: 100vh;
+  height: 100%;
+  width: 100%;
+
+  .text-boxes {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-left: 8rem;
+  }
+  .text-boxes .text-box {
+    display: flex;
+    flex-direction: column;
+    height: 350px;
+  }
+  .text-boxes .text-box .topic {
+    font-size: 18px;
+    font-weight: 600;
+    color: #fdfdfd;
+    margin: 4px;
+  }
+  .text-boxes .text-box textarea {
+    height: 50%;
+    width: 60vw !important;
+
+    border: 1px solid #2e2c2c;
+    border-radius: 8px;
+    background: #9095aa;
+  }
+  .text-box textarea::-webkit-scrollbar {
+    display: none;
+  }
+  .text-boxes .text-box button {
+    height: 45px;
+    width: 155px;
+    color: #fff;
+    background: #444549;
+    outline: none;
+    border: none;
+    border-radius: 8px;
+    font-size: 17px;
+    font-weight: 400;
+    margin: 8px 0;
+    cursor: pointer;
+    transition: all 0.4s ease;
+  }
+  .text-boxes .text-box button:hover {
+    background: #3a4153;
+  }
 
   @media screen and (max-width: 768px) {
     .left {
       margin-left: 0rem;
       margin-right: 0rem;
+    }
+    .text-boxes {
+      margin-left: 0rem;
+      margin-right: 0rem;
+      margin-left: 4vw;
+    }
+    .text-boxes .text-box textarea {
+      height: 50%;
+      width: 90vw !important ;
     }
   }
 `;
--- src/components/CodeCopyElement/index.js
+++ src/components/CodeCopyElement/index.js
@@ -7,7 +7,35 @@ const CodeCopyComponent = () => {
       <CodeCopyDiv>
         <div className="left">
           <h1> Base page CSS </h1>
+          <div class="text-boxes">
+            <div class="text-box JSBox">
+              <div class="topic">JavaScript Code:</div>
+              <textarea id="JSBox" readonly>
+                Add JavaScript Code here
+              </textarea>
+              <button id="JSButton">Copy Codes</button>
+            </div>
+          </div>
+          <div class="text-boxes">
+            <div class="text-box JSBox">
+              <div class="topic">JavaScript Code:</div>
+              <textarea id="JSBox" readonly>
+                Add JavaScript Code here
+              </textarea>
+              <button id="JSButton">Copy Codes</button>
+            </div>
+          </div>
+          <div class="text-boxes">
+            <div class="text-box JSBox">
+              <div class="topic">JavaScript Code:</div>
+              <textarea id="JSBox" >
+                Add JavaScript Code here
+              </textarea>
+              <button id="JSButton">Copy Codes</button>
+            </div>
+          </div>
         </div>
+        
       </CodeCopyDiv>
     </>
   );
